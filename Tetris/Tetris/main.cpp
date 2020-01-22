a7ed8e4c6405350b92b5e10496489af8e3322531
ZXWin <411184050@qq.com>
2020-01-23 00:23:49 +0800

    创建工程

---
+++ Tetris/Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by ZXWin on 2020/1/23.
+//  Copyright © 2020 Rough-Stone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
